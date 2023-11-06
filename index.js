bd54933f836824e63310d443c12512841dca149e
ismail-hossen <satict2016@gmail.com>
2023-11-06 14:29:03 +0600

    created api for get recent 6 blogs and for add blog

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middlewares
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 
 const client = new MongoClient(process.env.DB_URI, {
@@ -22,9 +22,19 @@ async function run() {
     const db = (await client.connect()).db("blog-post");
     const blogCollection = db.collection("blogs");
 
-    app.get("/blogs", async (req, res) => {
-      const result = await blogCollection.find().toArray();
+    app.get("/api/v1/recent-blogs", async (req, res) => {
+      const result = await blogCollection
+        .find()
+        .sort({ addedTime: 1 })
+        .limit(6)
+        .toArray();
       res.send(result);
+    });
+
+    app.post("/api/v1/add-blog", async (req, res) => {
+      const body = req.body;
+      const result = await blogCollection.insertOne(body);
+      res.status(200).send(result);
     });
 
     await client.db("admin").command({ ping: 1 });
