a988d4c497a08d7b304fc5ffbfe071cccb16d952
ismail-hossen <satict2016@gmail.com>
2023-11-06 11:09:03 +0600

    setup server and database and install express, mongodb, cors, dotenv in the server

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(process.env.DB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const db = (await client.connect()).db("blog-post");
+    const blogCollection = db.collection("blogs");
+
+    app.get("/blogs", async (req, res) => {
+      const result = await blogCollection.find().toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World! Welcome!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
